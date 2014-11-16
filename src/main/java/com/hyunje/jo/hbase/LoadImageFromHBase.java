fb0d8ecf837875d15a5a05e7a16d684368a88926
Hyunje Jo <retriever89@gmail.com>
2014-11-16 16:48:44 +0900

    changed byte[] type. completed load image from hbase

---
+++ src/main/java/com/hyunje/jo/hbase/LoadImageFromHBase.java
@@ -0,0 +1,73 @@
+package com.hyunje.jo.hbase;
+
+import com.hyunje.jo.hbase.util.PropertyLoader;
+import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Description
+ *
+ * @author hyunje
+ * @since 14. 11. 16.
+ */
+public class LoadImageFromHBase {
+    public static final String TableName = "Image";
+    public static final String imgPath = "result.jpg";
+    public static final String FileNameColumn = "Filename";
+    public static final String DataColumn = "Data";
+    public static final String DataQualifier = "data";
+
+    public static void main(String[] args) throws IOException, Base64DecodingException {
+        Properties hBaseProp = new PropertyLoader().getProperties();
+
+        Configuration configuration = HBaseConfiguration.create();
+        configuration.set("hbase.master", hBaseProp.getProperty("host"));
+        configuration.set("hbase.zookeeper.quorum", hBaseProp.getProperty("zooquorum"));
+        configuration.set("hbase.zookeeper.property.clientPort", hBaseProp.getProperty("zooport"));
+
+        HTableDescriptor imageTableDescriptor = new HTableDescriptor(org.apache.hadoop.hbase.TableName.valueOf(TableName));
+        imageTableDescriptor.addFamily(new HColumnDescriptor(FileNameColumn));
+        imageTableDescriptor.addFamily(new HColumnDescriptor(DataColumn));
+
+        System.out.println("Connecting");
+        HBaseAdmin admin = new HBaseAdmin(configuration);
+        HTable table = new HTable(configuration, TableName);
+
+        if (!admin.tableExists(table.getName())) {
+            System.err.println("Table is not exist");
+            System.exit(-1);
+        }
+
+        Get get = new Get(Bytes.toBytes("1"));
+
+        Result result = table.get(get);
+        if (result.isEmpty()) {
+            System.exit(-1);
+        }
+
+        byte[] imageBytes = result.getValue(Bytes.toBytes(DataColumn), Bytes.toBytes(DataQualifier));
+
+        System.out.println("File length : " + imageBytes.length);
+        System.out.println("Saving Image File : " + imgPath);
+
+
+        BufferedImage img = ImageIO.read(new ByteArrayInputStream(imageBytes));
+        ImageIO.write(img, "jpg", new File(imgPath));
+
+    }
+}
--- src/main/java/com/hyunje/jo/hbase/SaveImageToHBase.java
+++ src/main/java/com/hyunje/jo/hbase/SaveImageToHBase.java
@@ -12,8 +12,7 @@ import org.apache.hadoop.hbase.util.Bytes;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-import java.awt.image.WritableRaster;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
@@ -37,7 +36,7 @@ public class SaveImageToHBase {
         Configuration configuration = HBaseConfiguration.create();
         configuration.set("hbase.master", hBaseProp.getProperty("host"));
         configuration.set("hbase.zookeeper.quorum", hBaseProp.getProperty("zooquorum"));
-        configuration.set("hbase.zookeeper.property.clientPort",hBaseProp.getProperty("zooport"));
+        configuration.set("hbase.zookeeper.property.clientPort", hBaseProp.getProperty("zooport"));
 
 
         HTableDescriptor imageTableDescriptor = new HTableDescriptor(org.apache.hadoop.hbase.TableName.valueOf(TableName));
@@ -49,12 +48,13 @@ public class SaveImageToHBase {
         HTable table = new HTable(configuration, TableName);
 
         System.out.println("Create table if is not exist");
-        createIfNotExist(admin,imageTableDescriptor);
+        refreshOnExist(admin, imageTableDescriptor);
+//        createIfNotExist(admin,imageTableDescriptor);
 
         System.out.println("Loading Image");
         Put image = new Put(Bytes.toBytes("1"));
-        image.add(Bytes.toBytes(FileNameColumn), Bytes.toBytes(DataQualifier),Bytes.toBytes(imgPath));
-        image.add(Bytes.toBytes(DataColumn),Bytes.toBytes(DataQualifier), extractBytes(imgPath));
+        image.add(Bytes.toBytes(FileNameColumn), Bytes.toBytes(DataQualifier), Bytes.toBytes(imgPath));
+        image.add(Bytes.toBytes(DataColumn), Bytes.toBytes(DataQualifier), extractBytes(imgPath));
 
         System.out.println("Insert Image");
         table.put(image);
@@ -62,17 +62,25 @@ public class SaveImageToHBase {
 
     }
 
-    public static void createIfNotExist(HBaseAdmin admin, HTableDescriptor table) throws IOException{
-        if(!admin.tableExists(table.getName())){
+    public static void createIfNotExist(HBaseAdmin admin, HTableDescriptor table) throws IOException {
+        if (!admin.tableExists(table.getName())) {
             admin.createTable(table);
         }
+    }
+
+    public static void refreshOnExist(HBaseAdmin admin, HTableDescriptor table) throws IOException {
+        if (admin.tableExists(table.getName())) {
+            admin.disableTable(table.getName());
+            admin.deleteTable(table.getName());
+        }
+        admin.createTable(table);
     }
 
     public static byte[] extractBytes(String imagePath) throws IOException {
         File imageFile = new File(imagePath);
         BufferedImage image = ImageIO.read(imageFile);
-        WritableRaster raster = image.getRaster();
-        DataBufferByte data = (DataBufferByte) raster.getDataBuffer();
-        return data.getData();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ImageIO.write(image, "jpg", outputStream);
+        return outputStream.toByteArray();
     }
 }
